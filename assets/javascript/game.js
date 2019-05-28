ed245a03e13231b353cff22dea0b573561711ae1
Vincent Nguyen <vpvnguyen@gmail.com>
2019-05-28 11:10:16 -0700

    created file structure; html elements; game logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var correctLetter = ["v"];
+var wins = 0;
+var losses = 0;
+var guessLeft = 9;
+var guessLetter = [];
+
+// when user presses a key
+document.onkeyup = function (a) {
+    var userGuess = a.key;
+
+    // Start game logic
+    if (correctLetter.indexOf(userGuess) === -1) {
+        guessLeft--;
+        document.getElementById("guesses-left").innerHTML = guessLeft;
+        document.getElementById("losses").innerHTML = losses;
+        guessLetter.push(userGuess);
+        document.getElementById("guessed").innerHTML = guessLetter;
+    } else {
+        wins++; // lose a point
+        document.getElementById("wins").innerHTML = wins;
+    }
+}
